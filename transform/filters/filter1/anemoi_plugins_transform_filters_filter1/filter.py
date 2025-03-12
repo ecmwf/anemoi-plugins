2889627bad8c573dd3b86936ce18b02b53956321
Florian Pinault <Florian.Pinault@ecmwf.int>
2025-03-12 13:15:11 +0000

    qa

--- transform/filters/filter1/anemoi_plugins_transform_filters_filter1/filter.py
+++ transform/filters/filter1/anemoi_plugins_transform_filters_filter1/filter.py
@@ -3,11 +3,10 @@ from __future__ import annotations
 
 import logging
 
+# from anemoi.transform.filters import filter_registry
 from anemoi.transform.fields import new_field_from_numpy
 from anemoi.transform.fields import new_fieldlist_from_list
 from anemoi.transform.filter import Filter
-
-# from anemoi.transform.filters import filter_registry
 
 LOG = logging.getLogger(__name__)
 
